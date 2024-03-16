07f2cf4273b196be0c6834eab6b3e2451a7f14bd
Awesome <awesome_devnet@outlook.com>
2024-03-16 12:48:49 +0900

    feat: implement logging service

--- BotCore/LoggingService.cs
+++ BotCore/LoggingService.cs
@@ -1,4 +1,21 @@
-﻿namespace BotCore {
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BotCore {
     public class LoggingService {
+        public LoggingService(DiscordSocketClient client, CommandService command) {
+            client.Log += LogAsync;
+            command.Log += LogAsync;
+        }
+
+        private Task LogAsync(LogMessage msg) {
+            if(msg.Exception is CommandException cmdException) {
+                Console.WriteLine($"[Command/{msg.Severity}] {cmdException.Command.Aliases.First()}"
+                    + $" failed to execute in {cmdException.Context.Channel}.");
+                Console.WriteLine(cmdException);
+            } else Console.WriteLine($"[General/{msg.Severity}] {msg}");
+            return Task.CompletedTask;
+        }
     }
 }
