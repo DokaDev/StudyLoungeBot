e227d980e7f8dea54d619b881a5d3dba2a5fd44c
Awesome <awesome_devnet@outlook.com>
2024-03-16 12:49:38 +0900

    feat: implement basic connection and event handling logic

--- BotCore/CoreModule.cs
+++ BotCore/CoreModule.cs
@@ -2,7 +2,34 @@
 using Discord.WebSocket;
 
 namespace BotCore {
-    public class CoreModule {
+    public class CoreModule { 
+        public DiscordSocketClient? Client { get; private set; }
+        public CoreModule() {
+            Client = new DiscordSocketClient();
+        }
 
+        public async Task RunAsync() {
+            if(Client is null)
+                throw new InvalidOperationException("Client is null");
+
+            DiscordSocketConfig config = new() { MessageCacheSize = 100 };
+
+            // Token should be stored in a secure location
+            await Client.LoginAsync(TokenType.Bot, "token");
+            await Client.StartAsync();
+
+            // Handle events
+            Client.MessageUpdated += MessageUpdated;
+            Client.Ready += () => {
+                Console.WriteLine("Bot ready");
+                return Task.CompletedTask;
+            };
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel) {
+            // If the message was not in the cache, downloading it will result in getting a copy of the message.
+            IMessage msg = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{msg} -> {after}");
+        }
     }
 }
